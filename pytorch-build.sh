d53fe9396c16f0ae9e996b1aeced098c05f43252
Benjamin Glass <bglass@openteams.com>
2026-05-28 20:42:49 +0000

    Update build scripts
    1. Use pushd/popd rather than cd
    2. Remove obsolete build flags
    3. Stop nuking the torch-vision build directory on every build

--- pytorch-build.sh
+++ pytorch-build.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 eval "$(conda shell.bash hook)"
 conda activate ${PYTORCH_CONDA_ENV:=pytorch-dev$PYTORCH_BUILD_SUFFIX}
-cd ${PYTORCH_BUILD_DIRECTORY:=~/git$PYTORCH_BUILD_SUFFIX}/pytorch
+pushd ${PYTORCH_BUILD_DIRECTORY:=~/git$PYTORCH_BUILD_SUFFIX}/pytorch
 
 source $SCRIPT_DIR/torch-common.sh
 
@@ -15,3 +15,6 @@ pip install -e . --no-build-isolation -v $@
 
 # comment out if you're developing triton as well
 make triton
+
+# leave build directory
+popd
